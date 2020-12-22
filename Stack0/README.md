2760a0065847bbc395f233147566cb52839c3a76
incognito99 <shauqighost99@gmail.com>
2020-12-22 06:40:06 -0800

    stack0

---
+++ Stack0/README.md
@@ -0,0 +1,3 @@
+## STACK0
+
+1. This is quiet easy, we just try to put any input to overflow the applications
---
+++ updateGit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then 
+	echo "$0 <message to commit>"
+	exit 1
+fi
+
+git add .
+git commit -m $1
+git push -u origin main
